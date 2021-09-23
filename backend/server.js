28b6ca469d152f636df0969a26b485ec2799c1e3
daphnemagsby <daphnemagsby@gmail.com>
2021-09-22 20:37:22 -0500

    Set up database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const router = express.Router();
-const app = express();
-const port = process.env.PORT||3000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const entryRouter = require('./routes/entries');
 
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT||3000;
+
+
+mongoose.connect(process.env.MONGO_URI)
+	.then(()=> console.log('DB connected successfully'))
+	.catch(err=> console.log(`DB conenction error: ${err.message}`));
+
+app.use(express.json());
 app.use('/entries', entryRouter);
 
 app.get('/',(req,res)=>{
